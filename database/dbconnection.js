ac71c6a59ee9e40d7605e70b462de435447cc4e1
MohamedMedhat143 <120517976+MohamedMedhat143@users.noreply.github.com>
2025-07-07 01:27:28 +0300

    update the mongo connnection

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,6 +1,9 @@
 import { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const dbConn = connect("mongodb://localhost:27017/book")
+const mongoURI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster.rknbn.mongodb.net/${process.env.DATABASE_NAME}`;
+export const dbConn = connect(mongoURI)
   .then(() => {
     console.log("database connected*_*");
   })
