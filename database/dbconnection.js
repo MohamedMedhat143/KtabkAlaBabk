be471a0fffd0bf2e768c5ef36e714e0e346819fc
MohamedMedhat143 <120517976+MohamedMedhat143@users.noreply.github.com>
2025-07-09 09:05:40 +0300

    edit in the data base connection

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -2,7 +2,8 @@ import { connect } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoURI = `mongodb://localhost:27017/${process.env.DATABASE_NAME}`;
+const mongoURI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster.rknbn.mongodb.net/${process.env.DATABASE_NAME}`;
+const mongoURILocal = `mongodb://localhost:27017/${process.env.DATABASE_NAME}`;
 export const dbConn = connect(mongoURI)
   .then(() => {
     console.log("database connected*_*");
